35cfa08fee17499a3555698148fc3435186068ab
Jang7788 <138991488+Jang7788@users.noreply.github.com>
2025-09-19 12:18:03 +0700

    Return Login && Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Board from './board';
 import Nav from './nav';
+import Login from './component/login';
+import Register from './component/register';
 function App() {
   return (
     <div>
       <Nav />
       <Board />
+      <Register />
+      <Login />
     </div>
   );
 }
