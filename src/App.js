73b38d483df0c5b674e32e72c2d8c56df3ff8bef
seagram17901 <129320550+seagram17901@users.noreply.github.com>
2025-09-19 08:30:18 +0700

    Login
    post /api/login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Board from './board';
 import Nav from './nav';
-import Register from './component/register';
-import Login from './component/login';
 function App() {
   return (
     <div>
       <Nav />
       <Board />
-      <Register />
-      <Login />
     </div>
   );
 }
